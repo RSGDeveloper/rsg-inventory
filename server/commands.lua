f32c73db58a88ae4bf749f7f58e9ca165867a3d9
RexshackGaming <rexshackuk@gmail.com>
2024-09-29 21:20:21 +0100

    Update commands.lua

--- server/commands.lua
+++ server/commands.lua
@@ -105,21 +105,6 @@ RegisterCommand('inventory', function(source)
     if Player(source).state.inv_busy then return end
     local RSGPlayer = RSGCore.Functions.GetPlayer(source)
     if not RSGPlayer then return end
-    if not RSGPlayer or RSGPlayer.PlayerData.metadata['isdead'] or RSGPlayer.PlayerData.metadata['inlaststand'] or RSGPlayer.PlayerData.metadata['ishandcuffed'] then return end
-    RSGCore.Functions.TriggerClientCallback('rsg-inventory:client:vehicleCheck', source, function(inventory, class)
-        if not inventory then return OpenInventory(source) end
-        if inventory:find('trunk-') then
-            OpenInventory(source, inventory, {
-                slots = VehicleStorage[class] and VehicleStorage[class].trunkSlots or VehicleStorage.default.slots,
-                maxweight = VehicleStorage[class] and VehicleStorage[class].trunkWeight or VehicleStorage.default.maxWeight
-            })
-            return
-        elseif inventory:find('glovebox-') then
-            OpenInventory(source, inventory, {
-                slots = VehicleStorage[class] and VehicleStorage[class].gloveboxSlots or VehicleStorage.default.slots,
-                maxweight = VehicleStorage[class] and VehicleStorage[class].gloveboxWeight or VehicleStorage.default.maxWeight
-            })
-            return
-        end
-    end)
+    if not RSGPlayer or RSGPlayer.PlayerData.metadata['isdead'] or RSGPlayer.PlayerData.metadata['ishandcuffed'] then return end
+	if not inventory then return OpenInventory(source) end
 end, false)
